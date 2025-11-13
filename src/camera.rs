dec64dbd55c0b640d48d03f19bb1071a5ee76247
TitanTT10 <titan@tenwinkel.org>
2025-11-13 21:21:17 +0100

    Borrowed Camera logic from bevy example
    I copied the logic for a orbital camera from a bevy example into camera/input.rs. Now I will need to incorporate that logic into my own code. Also I added a resources.rs and a events.rs file to hopefully make organisation simpler.

--- src/camera.rs
+++ src/camera.rs
@@ -1,9 +1,28 @@
+mod input;
+
 use bevy::prelude::*;
+
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        todo!();
+        app.add_systems(Startup, setup_camera);
     }
+}
+
+
+#[derive(Component)]
+struct MainCamera;
+
+fn setup_camera(
+    mut cmd: Commands
+) {
+    cmd.spawn((
+        MainCamera,
+        Camera3d::default(),
+        Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
+    cmd.spawn(DirectionalLight::default());
 }
